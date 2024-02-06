f42fb7a4ab83885db2f646860ebf9f485c4ca90c
yokomac <yokomaku.connecta@gmail.com>
2024-02-07 07:51:05 +0900

    First commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,31 @@
-import React from "react";
-import { useCookies } from "react-cookie";
-import { useSelector, useDispatch } from "react-redux/es/exports";
-import { useHistory } from "react-router-dom";
-import  { signOut } from "../authSlice";
-import "./header.css";
+import { useCookies } from 'react-cookie';
+import { useSelector, useDispatch } from 'react-redux/es/exports';
+import { useNavigate } from 'react-router-dom';
+import { signOut } from '../authSlice';
+import './header.css';
 
 export const Header = () => {
-  const auth = useSelector((state) => state.auth.isSignIn)
+  const auth = useSelector((state) => state.auth.isSignIn);
   const dispatch = useDispatch();
-  const history = useHistory();
-  const [cookies, setCookie, removeCookie] = useCookies();
+  const navigate = useNavigate();
+  const [cookies, setCookie, removeCookie] = useCookies(); // eslint-disable-line no-unused-vars
+
   const handleSignOut = () => {
     dispatch(signOut());
-    removeCookie("token");
-    history.push("/signin");
-  }
+    removeCookie('token');
+    navigate.push('/signin');
+  };
 
   return (
     <header className="header">
       <h1>Todoアプリ</h1>
-      {auth ? <button onClick={handleSignOut} className="sign-out-button">サインアウト</button> : <></>}
+      {auth ? (
+        <button onClick={handleSignOut} className="sign-out-button">
+          サインアウト
+        </button>
+      ) : (
+        <></>
+      )}
     </header>
-  )
-}
+  );
+};
